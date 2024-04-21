95720951520ef6f5985f556e6b513894793078d3
Ceren Altunbas <crnaltunbas35@gmail.com>
2024-04-21 20:08:39 +0300

    Seaborn ile veri görselleştirme anlatıldı.

--- data_analysis_with_python_data_visualization.py
+++ data_analysis_with_python_data_visualization.py
@@ -165,6 +165,29 @@ plt.show()
 # 3 adet plot yapıldı ama 3ünü beraber çalıştırınca hata aldım tekrar bakılıp kontrol edilecek
 
 
+# SEABORN
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+df = sns.load_dataset("tips")
+df.head()
+
+df["sex"].value_counts()
+sns.countplot(x=df["sex"], data=df)
+plt.show()
+
+# Aynısının matplotlib-de komutu:
+df["sex"].value_counts().plt(kind='bar')
+plt.show()
+
+# Kategorik bir görselleştirme yapıldığında seaborn ve matplotlib kullanırız.
 
 
+# Sayısal değişken Görselleştirme
 
+# Kutu grafik gösterimi
+sns.boxplot(x=df["total_bill"])
+plt.show()
+
+df["total_bill"].hist()
+plt.show()
