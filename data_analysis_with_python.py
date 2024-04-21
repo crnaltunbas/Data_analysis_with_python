ae9727d519e30387d616c1ca1012f20e173e829d
Ceren Altunbas <crnaltunbas35@gmail.com>
2024-04-21 11:33:31 +0300

    Concat ve Merge yapısıyla birleştirme işlemleri anlatıldı.

--- data_analysis_with_python.py
+++ data_analysis_with_python.py
@@ -594,4 +594,36 @@ df.loc[:, df.columns.str.contains("age")].apply(standart_scaler).head()
 # YA DA
 # df.loc[:, ["age", "age2", "age3"]] = df.loc[:, df.columns.str.contains("age")].apply(standart_scaler).head()
 
+# Birleştirme (Join) İşlemleri
+
+import numpy as np
+import pandas as pd
+m = np.random.randint(1, 30, size=(5, 3))
+df1 = pd.DataFrame(m, columns=["var1", "var2","var3"])
+df2 = df1 + 99
+
+pd.concat([df1, df2])
+# concat fonksiyonu alt alta satırları birleştirme işlemi yapar.
+pd.concat([df1, df2], ignore_index=True)
+# ilk uyguladığımız komut indeksleri olduğu gibi birleştirince iki data frame-in indeksleri tekrar ettiğinden düzgün bir
+# indeksleme olmaz. Bunun için ignore_index=True eklemesi yapıldı ve indeksleme düzeltildi.
+
+
+# Merge ile Birleştirme İşlemleri
+
+df1 = pd.DataFrame({'employees': ['john', 'dennis', 'mark', 'maria'],
+                    'group': ['accounting', 'engineering', 'engineering', 'hr']})
+
+df2 = pd.DataFrame({'employees': [ 'mark', 'john', 'dennis', 'maria'],
+                    'start_date': [2010, 2009, 2014, 2019]})
+
+pd.merge(df1, df2)
+pd.merge(df1, df2, on="employees")
+# on ile neye göre birleştirme işlemi yapacağımızı belirtebiliriz. Yukarıdaki iki çıktıda aynı birleştirmeyi yapar.
+# Amaç : Her çalışanın müdürünün bilgisine erişmek istyoruz.
+
+df3 = pd.merge(df1, df2)
+df4 = pd.DataFrame({'group':['accounting', 'engineering', 'hr'],
+                    'manager': ['Caner', 'Mustafa', 'Berkcan']})
+pd.merge(df3, df4)
 
