49ad3a4740e5acafd1276c7044cbdbd37444f126
Ceren Altunbas <crnaltunbas35@gmail.com>
2024-04-21 14:43:49 +0300

    Kategorik Görselleştirme yapıldı.

---
+++ data_analysis_with_python_data_visualization.py
@@ -0,0 +1,26 @@
+# VERİ GÖRSELLEŞTİRME: MATPLOTLİB & SEABORN
+
+# MATPLOTLIB
+# Düşük seviye bir veri görselleştirme aracıdır.
+# Kategorik bir değişken varsa sütun grafiği ile görselleştiriyoruz. countplot ya da bar ile gerçekleştirilebilir.
+# Sayısal değişken varsa histogram(hist), boxplot ile veri görselleştirilir.
+
+# Veri görselleştirmesi için BI(Business Intelligence) araçları daha çok kullanılmaktadır. Power BI gibi.
+
+# Kategorik Veri Görselleştirme
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+df.head()
+
+df["sex"].value_counts().plot(kind='bar')
+plt.show()
+
+
+
+
+
