9ae816a603eb6e83f2e5b260eab6d19508926c1a
Ceren Altunbas <crnaltunbas35@gmail.com>
2024-04-21 14:53:30 +0300

    Sayısal Veri Görselleştirme yapıldı.

--- data_analysis_with_python_data_visualization.py
+++ data_analysis_with_python_data_visualization.py
@@ -21,6 +21,22 @@ df["sex"].value_counts().plot(kind='bar')
 plt.show()
 
 
+# Sayısal Veri Görselleştirme
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+df.head()
+
+plt.hist(df["age"])
+plt.show()
+
+plt.boxplot(df["fare"])
+plt.show()
+
 
 
 
