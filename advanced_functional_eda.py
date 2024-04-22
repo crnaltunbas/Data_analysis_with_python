7661ac5b084c6ffb192b03142070a56c9a639a41
Ceren Altunbas <crnaltunbas35@gmail.com>
2024-04-22 23:01:17 +0300

    sayısal Değişken Analizi yapıldı.

--- advanced_functional_eda.py
+++ advanced_functional_eda.py
@@ -141,3 +141,30 @@ def cat_summary(dataframe,col_name, plot=False):
 cat_summary(df, "adult_male", plot=True)
 
 
+
+# Sayısal Değişken Analizi (Analysis of Numerical Variables)
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+df.head()
+
+cat_cols = [col for col in df.columns if str(df[col].dtypes) in ["category", "object", "bool"]]
+num_but_cat = [col for col in df.columns if df[col].nunique() < 10 and df[col].dtypes in ["int64", "float64"]]
+cat_but_car = [col for col in df.columns if df[col].nunique() > 20 and str(df[col].dtypes) in ["category", "object"]]
+
+cat_cols = cat_cols + num_but_cat
+cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+
+df[["age","fare"]].describe().T
+
+num_cols = [col for col in df.columns if df[col].dtypes in ["int", "float"] ]
+num_cols = [col for col in num_cols if col not in cat_cols]
+
+# BU KISIM TEKRAR İZLENECEK ÇOK BİR ŞEY ANLAMADIM !!!
+
